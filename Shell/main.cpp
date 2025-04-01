0e5e663eaf262c54b6c0e6ea8d804226a2edf428
Bongsu Jeon <bs.jeon87@gmail.com>
2025-04-01 15:00:26 +0900

    [fix] Update the code

--- Shell/main.cpp
+++ Shell/main.cpp
@@ -8,16 +8,16 @@ public:
 };
 
 
-TEST(SSD_DRIVER, ssd_readwrite_normal) {
+TEST_F(SsdDriverFixture, ssd_readwrite_normal) {
 	ssdDriver.write(1, 0x12345678);
 	EXPECT_EQ(0x12345678, ssdDriver.read(1));
 }
 
-TEST(SSD_DRIVER, ssd_read_excpetion) {
+TEST_F(SsdDriverFixture, ssd_read_excpetion) {
 	EXPECT_THROW(ssdDriver.read(200), SsdException);
 }
 
-TEST(SSD_DRIVER, ssd_write_excpetion) {
+TEST_F(SsdDriverFixture, ssd_write_excpetion) {
 	EXPECT_THROW(ssdDriver.write(100, 0x12345678), SsdException);
 }
 
