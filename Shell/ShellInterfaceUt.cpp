800d5c2cc926abf4d66503c301161aaa3a76e31f
Bongsu Jeon <bs.jeon87@gmail.com>
2025-04-02 08:27:10 +0900

    [refact] Update the test code

--- Shell/ShellInterfaceUt.cpp
+++ Shell/ShellInterfaceUt.cpp
@@ -2,39 +2,33 @@
 #include "ShellInterface.cpp"
 #include <gmock/gmock.h>
 
-TEST(ShellInterfaceUt, parseReadTest) {
+class ShellInterfaceFixture : public testing::Test
+{
+public:
     ShellInterface shellInterface;
+};
 
+TEST_F(ShellInterfaceFixture, parseReadTest) {
     EXPECT_EQ(READ_COMMAND, shellInterface.parse("read"));
 };
 
-TEST(ShellInterfaceUt, parseWriteTest) {
-    ShellInterface shellInterface;
-
+TEST_F(ShellInterfaceFixture, parseWriteTest) {
     EXPECT_EQ(WRITE_COMMAND, shellInterface.parse("write"));
 };
 
 
-TEST(ShellInterfaceUt, parseExitTest) {
-    ShellInterface shellInterface;
-
+TEST_F(ShellInterfaceFixture, parseExitTest) {
     EXPECT_EQ(EXIT_COMMAND, shellInterface.parse("exit"));
 }
 
-TEST(ShellInterfaceUt, parseHelpTest) {
-    ShellInterface shellInterface;
-
+TEST_F(ShellInterfaceFixture, parseHelpTest) {
     EXPECT_EQ(HELP_COMMAND, shellInterface.parse("help"));
 }
 
-TEST(ShellInterfaceUt, parseFullwriteTest) {
-    ShellInterface shellInterface;
-
+TEST_F(ShellInterfaceFixture, parseFullwriteTest) {
     EXPECT_EQ(FULLWRITE_COMMAND, shellInterface.parse("fullwrite"));
 }
 
-TEST(ShellInterfaceUt, parseFullreadTest) {
-    ShellInterface shellInterface;
-
+TEST_F(ShellInterfaceFixture, parseFullreadTest) {
     EXPECT_EQ(FULLREAD_COMMAND, shellInterface.parse("fullread"));
 }
