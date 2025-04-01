97aabb17771318ea71bc730bd4a53c18943a5653
Bongsu Jeon <bs.jeon87@gmail.com>
2025-04-01 14:58:05 +0900

    [refactor] Update the code

--- Shell/main.cpp
+++ Shell/main.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
 #include "gmock/gmock.h"
 
-TEST(SSD_DRIVER, ssd_readwrite_normal) {
+class SsdDriverFixture : public testing::Test
+{
+public:
 	SsdDriver ssdDriver;
+};
+
+
+TEST(SSD_DRIVER, ssd_readwrite_normal) {
 	ssdDriver.write(1, 0x12345678);
 	EXPECT_EQ(0x12345678, ssdDriver.read(1));
 }
 
 TEST(SSD_DRIVER, ssd_read_excpetion) {
-	SsdDriver ssdDriver;
 	EXPECT_THROW(ssdDriver.read(200), SsdException);
 }
 
 TEST(SSD_DRIVER, ssd_write_excpetion) {
-	SsdDriver ssdDriver;
 	EXPECT_THROW(ssdDriver.write(100, 0x12345678), SsdException);
 }
 
 int main(int argc, char** argv) {
 	testing::InitGoogleMock(&argc, argv);
 	return RUN_ALL_TESTS();
-	return 0;
 }
