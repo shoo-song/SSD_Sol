e71f8328e71a932522c6dda0d1f7b5d005b37aaa
shoo-song <kotlm28@naver.com>
2025-04-02 15:49:17 +0900

    [ut] add invalid param check ut

--- SSD/UT/UT.cpp
+++ SSD/UT/UT.cpp
@@ -8,82 +8,55 @@ class SSDTestFixture : public Test
 {
 public:
 	SSDReadDriver SSDRead;
-	SSDWriteDriver *SSDWrite;
-	FileInterface* SSDIn;
+	SSDWriteDriver SSDWrite;
+	InputData InputParser;
 };
 
 class MockFile : public FileInterface {
 public:
 	MOCK_METHOD(bool, open, (const std::string& filename), ());
 };
-#if 0
-TEST_F(SSDTestFixture, FileOpen) {
-	MockFile file;
-	EXPECT_CALL(file, open("ssd_nand.txt")).WillRepeatedly(Return(true));
-
-	FileInterface ssdIO;
-	EXPECT_EQ(true, ssdIO.open("ssd_nand.txt"));
-}
-
-TEST_F(SSDTestFixture, FileOpenCheck) {
-	MockFile file;
-	EXPECT_CALL(file, open("ssd_nand.txt")).WillRepeatedly(Return(true));
-
-	FileInterface ssdIO;
-	EXPECT_EQ(true, ssdIO.isOpen());
-}
-
-TEST_F(SSDTestFixture, FileClose) {
-	MockFile file;
-    EXPECT_CALL(file, open("ssd_nand.txt")).WillRepeatedly(Return(false));
-
-	FileInterface ssdIO;
-	ssdIO.close();
-
-	EXPECT_EQ(false, ssdIO.isOpen());
-}
-
-// (invalide check) Read Wrie 공통부
 TEST_F(SSDTestFixture, invalidecheck1) {
-	// CMD 는 W, R 두개만 존재
-	int value = 0;
-	EXPECT_THAT(value, AnyOf(Eq('W'), Eq('R')));
+	//Invalid cmd type
+	char write_data[20] = "0x123456789";
+	EXPECT_THROW(InputParser.ReadInput('A', "10", write_data), std::exception);
 }
-
 TEST_F(SSDTestFixture, invalidecheck2) {
-	
-	// LBA 0 ~ 100 범위내 동작
-	int LBA = 0;
-	EXPECT_THAT(LBA, Ge(0));
-	EXPECT_THAT(LBA, Le(99));
+	//Invalid lba range
+	char write_data[20] = "0x123456789";
+	EXPECT_THROW(InputParser.ReadInput('r', "100", write_data), std::exception);
 }
-
 TEST_F(SSDTestFixture, invalidecheck3) {
-	// LBA 0 ~ 100 이외는 "ERROR message" 
+	//Invalid data size
+	char write_data[20] = "0x123456789";
+	EXPECT_THROW(InputParser.ReadInput('A', "10", write_data), std::exception);
 }
-
 TEST_F(SSDTestFixture, invalidecheck4) {
-	//Data 는 0x 포함하여 10자
+	//Invalid data format
+	char write_data[20] = "0x1x345678";
+	EXPECT_THROW(InputParser.ReadInput('A', "10", write_data), std::exception);
 }
-#endif
 TEST_F(SSDTestFixture, Read1) {
 	//적은적 없으면 0 으로 return
-	
+	std::string data1 = "0x00000000";
+	EXPECT_EQ(data1, SSDRead.DoRead(0)); 
 }
 TEST_F(SSDTestFixture, Read2) {
-	//SSD_output.txt 가 없으면 file 생성
-
+//Read 요청을 SSD_Output.txt 에 write하고, read 하여 write 내용이 읽혀야함
+	std::string data1 = "0xABABABAB";
+	SSDWrite.DoWrite(0, data1);
+	EXPECT_EQ(data1, SSDRead.DoRead(0));
 }
 TEST_F(SSDTestFixture, Read3) {
-//Read 요청을 SSD_Output.txt 에 write하고, read 하여 write 내용이 읽혀야함
-}
-TEST_F(SSDTestFixture, Read4) {
 // 임이 LBA (0~99) 수행 시, LBA 검색하여 data read
+	std::string data1 = "0xABABABAB";
+	int LBA = 10;
+	SSDWrite.DoWrite(LBA, data1);
+	EXPECT_EQ(data1, SSDRead.DoRead(LBA));
 }
 
 TEST_F(SSDTestFixture, ReadAfterWrite) {
 	std::string data = "0x12345678";
-	SSDWriteDriver SSDWrite;
 	SSDWrite.DoWrite(0x0, data);
 
 	EXPECT_EQ(data, SSDRead.DoRead(0x0));
@@ -91,7 +64,6 @@ TEST_F(SSDTestFixture, ReadAfterWrite) {
 
 TEST_F(SSDTestFixture, WriteSameLBA) {
 	std::string data1 = "0x12345678";
-	SSDWriteDriver SSDWrite;
 	SSDWrite.DoWrite(0x0, data1);
 
 	std::string data2 = "0x15678000";
@@ -109,7 +81,6 @@ TEST_F(SSDTestFixture, WriteSameLBA) {
 
 TEST_F(SSDTestFixture, CheckReadFile) {
 	std::string data1 = "0x12345678";
-	SSDWriteDriver SSDWrite;
 	SSDWrite.DoWrite(0x0, data1);
 	SSDRead.DoRead(0);
 
