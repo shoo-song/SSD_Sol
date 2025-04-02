a851d444a37caedacc6b10e491f59a9b06129162
Bongsu Jeon <bs.jeon87@gmail.com>
2025-04-03 06:53:26 +0900

    [feat] Merge the Executor with shell

---
+++ Shell/Shell.cpp
@@ -0,0 +1,35 @@
+#pragma once
+#include "ShellExecutor.cpp"
+#include "ssddriver.h"
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cin;
+using std::cout;
+
+class Shell {
+public:
+    Shell() {
+        mExcutor.setDriverInterface(&mSsdDriver);
+    }
+    void runShell() {
+        while (true) {
+            try {
+                string input;
+                cout << "Shell > ";
+                std::getline(std::cin, input);
+
+                cout << mExcutor.execute(input) << "\n";
+            }
+            catch (exception e) {
+                std::cout << "INVALID COMMAND\n";
+            }
+        }
+    }
+private:
+    ShellExecutor mExcutor;
+    SsdDriver mSsdDriver;
+
+
+};
