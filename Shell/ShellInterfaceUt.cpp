72e93fa21ca1c3a3e6992b64a0dd656bbf2bcef2
Bongsu Jeon <bs.jeon87@gmail.com>
2025-04-02 09:32:31 +0900

    [ut] Add the ut for tdd

--- Shell/ShellInterfaceUt.cpp
+++ Shell/ShellInterfaceUt.cpp
@@ -1,11 +1,16 @@
 #pragma once
 #include "ShellInterface.cpp"
 #include <gmock/gmock.h>
+#include <vector>
+
+using namespace std;
 
 class ShellInterfaceFixture : public testing::Test
 {
 public:
     ShellInterface shellInterface;
+    vector<int> expectedArgs;
+    vector<string> inputArgs;
 };
 
 TEST_F(ShellInterfaceFixture, parseReadTest) {
@@ -30,4 +35,36 @@ TEST_F(ShellInterfaceFixture, parseFullwriteTest) {
 
 TEST_F(ShellInterfaceFixture, parseFullreadTest) {
     EXPECT_EQ(FULLREAD_COMMAND, shellInterface.parse("fullread"));
+}
+
+TEST_F(ShellInterfaceFixture, parseArgTest1) {
+    vector<int> convertedArgs;
+
+    inputArgs.push_back("1");
+    expectedArgs.push_back(1);
+
+    convertedArgs = shellInterface.convertCmdArgs(READ_COMMAND, inputArgs);
+
+    EXPECT_EQ(expectedArgs[0], convertedArgs[0]);
+    EXPECT_EQ(expectedArgs.size(), convertedArgs.size());
+}
+
+TEST_F(ShellInterfaceFixture, readCmdParseArgTest2) {
+    inputArgs.push_back("200");
+    EXPECT_THROW(shellInterface.convertCmdArgs(READ_COMMAND, inputArgs), ShellArgConvertException);
+}
+
+TEST_F(ShellInterfaceFixture, readCmdParseArgTest3) {
+    inputArgs.push_back("11");
+    inputArgs.push_back("20");
+    EXPECT_THROW(shellInterface.convertCmdArgs(READ_COMMAND, inputArgs), ShellArgConvertException);
+}
+
+TEST_F(ShellInterfaceFixture, readCmdParseArgTest4) {
+    EXPECT_THROW(shellInterface.convertCmdArgs(READ_COMMAND, inputArgs), ShellArgConvertException);
+}
+
+TEST_F(ShellInterfaceFixture, readCmdParseArgTest5) {
+    inputArgs.push_back("aaa");
+    EXPECT_EQ(false, shellInterface.convertCmdArgs(READ_COMMAND, inputArgs));
 }
