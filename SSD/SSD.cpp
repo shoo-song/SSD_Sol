bf4f9875ae535d7deb4024597ceb7f5d0c8160a8
shoo-song <kotlm28@naver.com>
2025-04-02 13:43:33 +0900

    [feat] ssd fileobj add

--- SSD/SSD.cpp
+++ SSD/SSD.cpp
@@ -1,70 +1,78 @@
 #include "gmock/gmock.h"
 #include "File_Interface.cpp"
-#include <fcntl.h>
 #include <iostream>
 using namespace std;
-#define BYTE_PER_LBA (10)
-class ReadDriver {
+class InputData {
 public:
+	void ReadInput(char RorW, string LBAstring, char* data = NULL) {
+		if ((RorW == 'W') || (RorW == 'w')) {
+			bIsWrite = true;
+		}
+		else if ((RorW == 'R') || (RorW == 'r')) {
+			bIsWrite = false;
+		}
+		else {
+			cout << "R or W needed\n";
+		}
+		LBA = stoi(LBAstring);
+		if (LBA >= 100) {
+			cout << "LBA overflow\n";
+		}
+		if (data != NULL) {
+			strcpy_s(input_data, data);
+		}
+	}
+	bool IsWrite(void) {
+		return bIsWrite;
+	}
+	int GetLBA(void) {
+		return LBA;
+	}
+	char* GetData(void) {
+		return input_data;
+	}
+private:
+	bool bIsWrite = false;
+	int LBA = 0;
+	char input_data[20] = {};
 };
-class WriteDriver {
+class SSDReadDriver {
 public:
+	void DoRead(int LBA) {
+		FileObj.read(LBA);
+	}
+private:
+	FileInterface FileObj;
+};
+class SSDWriteDriver {
+public:
+	void DoWrite(int LBA, char* data) {
+		FileObj.write(LBA, data);
+	}
+private:
+	FileInterface FileObj;
 };
 
 #ifndef UNIT_TEST
 int main(int argc, char* argv[]) {
-	char filename_nand[100] = "ssd_nand.txt";
-	char filename_output[100] = "ssd_output.txt";
-	char read_buf[20] = "00000000";
-	char write_buf[20] = "00000000";
-	char ReadOrWrite = 0;
-	int LBA = 0;
-	int nand_fd;
-	int result_fd;
-	int bytes_write;
-	int bytes_read;
-	//check param
-	ReadOrWrite = *argv[1];
-	LBA = stoi(argv[2]);
-	if ((ReadOrWrite == 'W') || (ReadOrWrite == 'w')) {
-		strcpy_s(write_buf,argv[3]);
+	InputData InputParam;
+	if (argc == 3) {
+		InputParam.ReadInput(*argv[1], argv[2]);
 	}
-	// parsing end
-	FileInterface NandFileForWrite;
-	FileInterface NandFileForRead;
-	FileInterface OutputFileForWrite;
-	bool bNeedFormat = false;
-	if (_access(filename_nand, O_RDWR) == -1) {
-		bNeedFormat = true;
-		cout << "need format" << endl;
-	}
-
-	if (bNeedFormat == true) {
-		NandFileForWrite.open(filename_nand, true/*IsFormat*/, true/*IsOpenForWrite*/);
-		for (int formatLBA = 0; formatLBA < 100; formatLBA++) {
-			NandFileForWrite.write(write_buf, formatLBA * BYTE_PER_LBA);
-		}
-		NandFileForWrite.close();
-	}
-
-	if ((ReadOrWrite == 'W') || (ReadOrWrite == 'w')) {
-		NandFileForWrite.open(filename_nand, false, true/*IsOpenForWrite*/);
-		NandFileForWrite.write(write_buf, LBA * BYTE_PER_LBA);
-		NandFileForWrite.close();
-	}
-	else if ((ReadOrWrite == 'R') || (ReadOrWrite == 'r')) {
-		NandFileForRead.open(filename_nand, false, false/*IsOpenForWrite*/);
-		NandFileForRead.read(read_buf, LBA * BYTE_PER_LBA);
-		NandFileForRead.close();
-
-		OutputFileForWrite.open(filename_output, true, true/*IsOpenForWrite*/);
-		OutputFileForWrite.write(read_buf, 0);
-		OutputFileForWrite.close();
+	else if (argc == 4) {
+		InputParam.ReadInput(*argv[1], argv[2], argv[3]);
 	}
 	else {
-		cout << "Read or Write needed" << endl;
+		cout << "Invalid argument count\n";
 	}
-	
+	SSDReadDriver Reader;
+	SSDWriteDriver Writer;
+	if (InputParam.IsWrite() == true) {
+		Writer.DoWrite(InputParam.GetLBA(), InputParam.GetData());
+	}
+	else {
+		Reader.DoRead(InputParam.GetLBA());
+	}
 	return 0;
 }
 #endif
