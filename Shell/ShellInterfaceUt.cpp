70d7ff269b376dd4ecc5c3b73d8c3151fdf0fca6
Bongsu Jeon <bs.jeon87@gmail.com>
2025-04-02 13:32:39 +0900

    [refact] Change the UT to use the mockDriver

--- Shell/ShellInterfaceUt.cpp
+++ Shell/ShellInterfaceUt.cpp
@@ -36,16 +36,22 @@ TEST_F(ShellInterfaceFixture, parseFullreadTest) {
     EXPECT_EQ(FULLREAD_COMMAND, ShellInterfaceUtil::getUtilObj().parse("fullread"));
 }
 
-
 TEST_F(ShellInterfaceFixture, readWriteTest1) {
+    MockSsdDriver mockDriver;
+    shellInterface.setDriverInterface(&mockDriver);
+
     EXPECT_EQ("[Write] Done", shellInterface.execute("write 01 0xAAAABBBB"));
     EXPECT_EQ("[Read] LBA 00 : 0xAAAABBBB", shellInterface.execute("read 01"));
 }
 
 TEST_F(ShellInterfaceFixture, readTest1) {
+    MockSsdDriver mockDriver;
+    shellInterface.setDriverInterface(&mockDriver);
     EXPECT_THROW(shellInterface.convertCmdArgs(shellInterface.execute("read 200"), ShellArgConvertException);
 }
 
 TEST_F(ShellInterfaceFixture, writeTest1) {
+    MockSsdDriver mockDriver;
+    shellInterface.setDriverInterface(&mockDriver);
     EXPECT_THROW(shellInterface.convertCmdArgs(shellInterface.execute("write 200 200"), ShellArgConvertException);
 }
