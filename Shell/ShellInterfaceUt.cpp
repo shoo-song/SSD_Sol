0e89e544a5eb2c8e6eb3650028ae2cf4b0b2e9e7
Bongsu Jeon <bs.jeon87@gmail.com>
2025-04-02 15:44:52 +0900

    [ut] Add Tcs for script

--- Shell/ShellInterfaceUt.cpp
+++ Shell/ShellInterfaceUt.cpp
@@ -2,8 +2,11 @@
 #include "ShellExecutor.cpp"
 #include "mock_ssddriver.h"
 #include <gmock/gmock.h>
+#include <gtest/gtest.h>
 #include <vector>
+#include <unordered_map>
 
+using namespace testing;
 using namespace std;
 
 class ShellExecutorFixture : public testing::Test
@@ -62,4 +65,60 @@ TEST_F(ShellExecutorFixture, writeTest1) {
     shellExecutor.setDriverInterface(&mockDriver);
 
     EXPECT_THROW(shellExecutor.execute("write 200 200"), ShellArgConvertException);
+}
+
+TEST_F(ShellExecutorFixture, script1_fullmatching) {
+    MockSsdDriver mockDriver;
+    shellExecutor.setDriverInterface(&mockDriver);
+
+    std::unordered_map<int, int> memory;
+
+    // write 호출 시 해당 위치에 데이터를 저장
+    ON_CALL(mockDriver, writeSSD(::testing::_, ::testing::_))
+        .WillByDefault(::testing::Invoke([&memory](int pos, int data) {
+        memory[pos] = data;
+            }));
+
+    // read 호출 시 해당 위치의 데이터를 반환
+    ON_CALL(mockDriver, readSSD(::testing::_))
+        .WillByDefault(::testing::Invoke([&memory](int pos) -> int {
+        auto it = memory.find(pos);
+        if (it != memory.end()) {
+            return it->second;
+        }
+        return -1; // 해당 위치에 데이터가 없을 경우
+            }));
+
+    shellExecutor.execute("1_FullWriteAndReadCompare");
+
+    EXPECT_CALL(mockDriver, writeSSD(::testing::_, ::testing::_)).Times(14);
+    EXPECT_CALL(mockDriver, readSSD(::testing::_)).Times(14);;
+}
+
+TEST_F(ShellExecutorFixture, script1_wildcard) {
+    MockSsdDriver mockDriver;
+    shellExecutor.setDriverInterface(&mockDriver);
+
+    std::unordered_map<int, int> memory;
+
+    // write 호출 시 해당 위치에 데이터를 저장
+    ON_CALL(mockDriver, writeSSD(::testing::_, ::testing::_))
+        .WillByDefault(::testing::Invoke([&memory](int pos, int data) {
+        memory[pos] = data;
+            }));
+
+    // read 호출 시 해당 위치의 데이터를 반환
+    ON_CALL(mockDriver, readSSD(::testing::_))
+        .WillByDefault(::testing::Invoke([&memory](int pos) -> int {
+        auto it = memory.find(pos);
+        if (it != memory.end()) {
+            return it->second;
+        }
+        return -1; // 해당 위치에 데이터가 없을 경우
+            }));
+
+    shellExecutor.execute("1_");
+
+    EXPECT_CALL(mockDriver, writeSSD(::testing::_, ::testing::_)).Times(14);
+    EXPECT_CALL(mockDriver, readSSD(::testing::_)).Times(14);
 }
--- Shell/ShellUtil.cpp
+++ Shell/ShellUtil.cpp
@@ -38,6 +38,28 @@ public:
 		return UNKOWN;
 	}
 
+	ShellCommand parseScript(string commandArg) {
+		if (commandArg.compare("read") == 0) {
+			return READ_COMMAND;
+		}
+		if (commandArg.compare("write") == 0) {
+			return WRITE_COMMAND;
+		}
+		if (commandArg.compare("help") == 0) {
+			return HELP_COMMAND;
+		}
+		if (commandArg.compare("exit") == 0) {
+			return EXIT_COMMAND;
+		}
+		if (commandArg.compare("fullwrite") == 0) {
+			return FULLWRITE_COMMAND;
+		}
+		if (commandArg.compare("fullread") == 0) {
+			return FULLREAD_COMMAND;
+		}
+		return UNKOWN;
+	}
+
 	// LBA 문자열 변환 (10진수, 0~99)
 	unsigned int convertDecimalStringForLba(const std::string& input) {
 		size_t pos;
