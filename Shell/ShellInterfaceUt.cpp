16abdebc907da71229f74e1527d59f6d58fe9ac5
Bongsu Jeon <bs.jeon87@gmail.com>
2025-04-02 13:29:57 +0900

    [refact] Change the UT

--- Shell/ShellInterfaceUt.cpp
+++ Shell/ShellInterfaceUt.cpp
@@ -9,69 +9,43 @@ class ShellInterfaceFixture : public testing::Test
 {
 public:
     ShellInterface shellInterface;
-    vector<int> expectedArgs;
-    vector<string> inputArgs;
+    string result;
 };
 
 TEST_F(ShellInterfaceFixture, parseReadTest) {
-    EXPECT_EQ(READ_COMMAND, shellInterface.parse("read"));
+    EXPECT_EQ(READ_COMMAND, ShellInterfaceUtil::getUtilObj().parse("read"));
 };
 
 TEST_F(ShellInterfaceFixture, parseWriteTest) {
-    EXPECT_EQ(WRITE_COMMAND, shellInterface.parse("write"));
+    EXPECT_EQ(WRITE_COMMAND, ShellInterfaceUtil::getUtilObj().parse("write"));
 };
 
 TEST_F(ShellInterfaceFixture, parseExitTest) {
-    EXPECT_EQ(EXIT_COMMAND, shellInterface.parse("exit"));
+    EXPECT_EQ(EXIT_COMMAND, ShellInterfaceUtil::getUtilObj().parse("exit"));
 }
 
 TEST_F(ShellInterfaceFixture, parseHelpTest) {
-    EXPECT_EQ(HELP_COMMAND, shellInterface.parse("help"));
+    EXPECT_EQ(HELP_COMMAND, ShellInterfaceUtil::getUtilObj().parse("help"));
 }
 
 TEST_F(ShellInterfaceFixture, parseFullwriteTest) {
-    EXPECT_EQ(FULLWRITE_COMMAND, shellInterface.parse("fullwrite"));
+    EXPECT_EQ(FULLWRITE_COMMAND, ShellInterfaceUtil::getUtilObj().parse("fullwrite"));
 }
 
 TEST_F(ShellInterfaceFixture, parseFullreadTest) {
-    EXPECT_EQ(FULLREAD_COMMAND, shellInterface.parse("fullread"));
+    EXPECT_EQ(FULLREAD_COMMAND, ShellInterfaceUtil::getUtilObj().parse("fullread"));
 }
 
-TEST_F(ShellInterfaceFixture, parseArgTest1) {
-    vector<unsigned int> convertedArgs;
 
-    inputArgs.push_back("01");
-    expectedArgs.push_back(1);
-
-    convertedArgs = shellInterface.convertCmdArgs(READ_COMMAND, inputArgs);
-
-    EXPECT_EQ(expectedArgs[0], convertedArgs[0]);
-    EXPECT_EQ(expectedArgs.size(), convertedArgs.size());
+TEST_F(ShellInterfaceFixture, readWriteTest1) {
+    EXPECT_EQ("[Write] Done", shellInterface.execute("write 01 0xAAAABBBB"));
+    EXPECT_EQ("[Read] LBA 00 : 0xAAAABBBB", shellInterface.execute("read 01"));
 }
 
-TEST_F(ShellInterfaceFixture, readCmdParseArgTest2) {
-    inputArgs.push_back("200");
-    EXPECT_THROW(shellInterface.convertCmdArgs(READ_COMMAND, inputArgs), ShellArgConvertException);
+TEST_F(ShellInterfaceFixture, readTest1) {
+    EXPECT_THROW(shellInterface.convertCmdArgs(shellInterface.execute("read 200"), ShellArgConvertException);
 }
 
-TEST_F(ShellInterfaceFixture, readCmdParseArgTest3) {
-    inputArgs.push_back("11");
-    inputArgs.push_back("20");
-    EXPECT_THROW(shellInterface.convertCmdArgs(READ_COMMAND, inputArgs), ShellArgConvertException);
-}
-
-TEST_F(ShellInterfaceFixture, writeParseArgTest1) {
-    vector<unsigned int> convertedArgs;
-
-    inputArgs.push_back("1");
-    inputArgs.push_back("0x12345678");
-
-    expectedArgs.push_back(1);
-    expectedArgs.push_back(0x12345678);
-
-    convertedArgs = shellInterface.convertCmdArgs(WRITE_COMMAND, inputArgs);
-
-    EXPECT_EQ(expectedArgs[0], convertedArgs[0]);
-    EXPECT_EQ(expectedArgs[1], convertedArgs[1]);
-    EXPECT_EQ(expectedArgs.size(), convertedArgs.size());
+TEST_F(ShellInterfaceFixture, writeTest1) {
+    EXPECT_THROW(shellInterface.convertCmdArgs(shellInterface.execute("write 200 200"), ShellArgConvertException);
 }
