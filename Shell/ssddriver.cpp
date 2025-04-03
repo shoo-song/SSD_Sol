120b50329f51a436ae1d9c79bf3b5ecfccf4f5f1
Bongsu Jeon <bs.jeon87@gmail.com>
2025-04-03 10:03:42 +0900

    [fix] Fix the driver code

--- Shell/ssddriver.cpp
+++ Shell/ssddriver.cpp
@@ -1,22 +1,33 @@
 #include "ssddriver.h"
 #include "config.h"
+#include "ShellUtil.cpp"
 #include <fstream>
 #include <sstream>
 #include <string>
 #include <cstdlib>
+#include <windows.h>  // Windows API 사용
 
 bool SsdDriver::executeCmd(std::string cmdLine) {
-    int result = system(cmdLine.c_str());
+    char path[MAX_PATH];  // 실행 파일 전체 경로를 저장할 버퍼
+    GetModuleFileNameA(NULL, path, MAX_PATH);  // 현재 실행 파일의 전체 경로 가져오기
+
+    std::string exePath = path;  // std::string으로 변환
+    size_t pos = exePath.find_last_of("\\");  // 마지막 '\'의 위치 찾기
+    std::string dirPath = exePath.substr(0, pos);  // 실행 파일이 있는 디렉터리 경로
+
+    // 실행할 command.exe 경로 설정
+    std::string newCmd = dirPath + "\\" + cmdLine;
+
+    int result = system(newCmd.c_str());
 
     if (result) {
-        return true;
+        return false;
     }
-    return false;
+    return true;
 }
 
 uint32_t SsdDriver::readSSD(int LBA) {
-
-    std::string cmd = "SSD.exe R " + std::to_string(LBA);
+    std::string cmd = "SSD.exe R " + ShellUtil::getUtilObj().toTwoDigitString(LBA);
     const char* cmdLine= cmd.c_str();
 
     executeCmd(cmdLine);
@@ -24,20 +35,22 @@ uint32_t SsdDriver::readSSD(int LBA) {
     uint32_t ret = 0xFFFFFFFF;
 
     std::ifstream file(config::FILE_PATH); 
-    if (!file.is_open()) return ret;
-
-    std::string line;
-    int current = 1;
-    while (std::getline(file, line)) {
-        if (current == LBA) return ConvertStrToUint32(line);
-        ++current;
+    if (!file.is_open())
+    {
+        //throw
+        return ret;
     }
+
+    std::string outputData;
+    std::getline(file, outputData);
+    ret = ShellUtil::getUtilObj().convertHexStringForData(outputData);
 
     return ret;
 }
 
 void SsdDriver::writeSSD(int LBA, uint32_t data) {
-    std::string cmd = "SSD.exe W " + std::to_string(LBA) + ' ' + std::to_string(data);
+    std::string cmd = "SSD.exe W " + ShellUtil::getUtilObj().toTwoDigitString(LBA) + " "
+        + ShellUtil::getUtilObj().toHexFormat(data);
     const char* cmdLine = cmd.c_str();
 
     executeCmd(cmdLine);
