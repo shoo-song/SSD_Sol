a8b5a07f54417b88779daf740394f62781855c88
shoo-song <kotlm28@naver.com>
2025-04-03 17:04:43 +0900

    [feat] cmd merge enable

--- SSD/main.cpp
+++ SSD/main.cpp
@@ -2,39 +2,39 @@
 #include <iostream>
 #include "SSD.cpp"
 #include "FileManager.h"
-#include "command_parse.h"
+#include "command_parser.h"
 #define UNIT_TEST
 using namespace std;
-bool commandParse(int argc, SSDCommand& InputParam, char* argv[])
+bool commandParse(int argc, CommandParser& parser, char* argv[])
 {
 	if (argc == 3) {
-		return InputParam.parseArg(*argv[1], argv[2]);
+		return parser.parseArg(*argv[1], argv[2]);
 	}
 	else if (argc == 4) {
-		return InputParam.parseArg(*argv[1], argv[2], argv[3]);
+		return parser.parseArg(*argv[1], argv[2], argv[3]);
 	}
 	return false;
 }
-void executeCMD(SSDCommand& InputParam)
+void executeCMD(CommandParser parser)
 {
 	SSD MySSD;
-	if (InputParam.GetCmdType() == CMD_WRITE) {
-		MySSD.DoWrite(InputParam.GetLBA(), InputParam.GetData());
+	if (parser.GetCmdType() == CMD_WRITE) {
+		MySSD.DoWrite(parser.GetLBA(), parser.GetData());
 	}
-	else if (InputParam.GetCmdType() == CMD_READ) {
-		MySSD.DoRead(InputParam.GetLBA());
+	else if (parser.GetCmdType() == CMD_READ) {
+		MySSD.DoRead(parser.GetLBA());
 	}
-	else if (InputParam.GetCmdType() == CMD_ERASE) {
-		MySSD.DoErase(InputParam.GetLBA(), InputParam.GetEraseCount());
+	else if (parser.GetCmdType() == CMD_ERASE) {
+		MySSD.DoErase(parser.GetLBA(), parser.GetEraseEndLBA() - parser.GetLBA() + 1);
 	}
 }
 #ifndef UNIT_TEST
 int main(int argc, char* argv[]) {
-	SSDCommand InputParam;
+	CommandParser parser;
 	bool invalid = false;
-	invalid = commandParse(argc, InputParam, argv);
+	invalid = commandParse(argc, parser, argv);
 	if (invalid == false) return 0;
-	executeCMD(InputParam);
+	executeCMD(parser.GetExcuteCMD());
 
 	return 0;
 }
