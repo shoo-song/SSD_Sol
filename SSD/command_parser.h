70905546206bb39b4474d739f6a9b96e81eba69f
shoo-song <kotlm28@naver.com>
2025-04-04 10:05:47 +0900

    [fix] erase cmd fix

--- SSD/command_parser.h
+++ SSD/command_parser.h
@@ -3,9 +3,9 @@
 #define CMD_WRITE (0)
 #define CMD_READ (1)
 #define CMD_ERASE (2)
-#define MAX_LBA (99)
+#define MAX_LBA_COUNT (100)
 #define MAX_ERASE_SIZE (10)
-
+using namespace std;
 struct CmdInfo {
 	char CMDType;
 	string LBA;
@@ -16,7 +16,7 @@ struct SSDCommand {
 	int CMDType = 0;
 	int LBA = 0;
 	int EraseEndLBA = 0;
-	bool IsInvalid = 1;
+	bool IsValid = false;
 	char input_data[20] = {};
 };
 class CMDBuffer {
@@ -25,19 +25,27 @@ public:
 	void AppendCMD(SSDCommand InputParam) {
 		// append
 		cmd_array[empty_buffer_idx] = InputParam;
-		cmd_array[empty_buffer_idx++].IsInvalid = false;
+		cmd_array[empty_buffer_idx++].IsValid = true;
 		MergeCMD();
 	}
 	int CheckValidCmdCount(void) {
 		int result = 0;
 		for (int nIter = 0; nIter < 5; nIter++) {
-			if (cmd_array[nIter].IsInvalid == false) {
+			if (cmd_array[nIter].IsValid == true) {
 				result++;
 			}
 		}
 		return result;
 	}
 private:
+	void MergeEraseRange(SSDCommand* prevCMD, SSDCommand* curCMD) {
+		int EraseCount = max(curCMD->EraseEndLBA, prevCMD->EraseEndLBA) - min(curCMD->LBA, prevCMD->LBA) + 1;
+		if (EraseCount <= MAX_ERASE_SIZE) {
+			curCMD->LBA = min(curCMD->LBA, prevCMD->LBA);
+			curCMD->EraseEndLBA = max(curCMD->EraseEndLBA, prevCMD->EraseEndLBA);
+			prevCMD->IsValid = false;
+		}
+	}
 	void MergeCMD(void) {
 		for (int cmd_offset = 1; cmd_offset < empty_buffer_idx; cmd_offset++) {
 			SSDCommand* prevCMD = &cmd_array[empty_buffer_idx - 1 - cmd_offset];
@@ -48,7 +56,7 @@ private:
 				if (prevCMD->CMDType == CMD_WRITE) {
 					// W-W
 					if (curCMD->LBA == prevCMD->LBA) {
-						prevCMD->IsInvalid = true;
+						prevCMD->IsValid = false;
 					}
 				}
 				else {
@@ -56,13 +64,13 @@ private:
 					if (curCMD->LBA == prevCMD->EraseEndLBA) {
 						prevCMD->EraseEndLBA--;
 						if (prevCMD->EraseEndLBA < prevCMD->LBA) {
-							prevCMD->IsInvalid = true;
+							prevCMD->IsValid = false;
 						}
 					}
 					else if (curCMD->LBA == prevCMD->LBA) {
 						prevCMD->LBA++;
 						if (prevCMD->LBA > prevCMD->EraseEndLBA) {
-							prevCMD->IsInvalid = true;
+							prevCMD->IsValid = false;
 						}
 					}
 				}
@@ -71,33 +79,30 @@ private:
 				if (prevCMD->CMDType == CMD_WRITE) {
 					// W-E
 					if (((prevCMD->LBA) >= (curCMD->LBA)) && ((prevCMD->LBA) <= (curCMD->EraseEndLBA))) {
-						prevCMD->IsInvalid = true;
+						prevCMD->IsValid = false;
 					}
 				}
 				else {
 					// E-E
-					if ((prevCMD->LBA > curCMD->LBA) && (prevCMD->LBA <= curCMD->EraseEndLBA)) {
-						curCMD->EraseEndLBA = prevCMD->EraseEndLBA;
-						prevCMD->IsInvalid = true;
-					}
-					else if ((prevCMD->LBA <= curCMD->LBA) && (prevCMD->EraseEndLBA >= curCMD->LBA)) {
-						curCMD->LBA = prevCMD->LBA;
-						prevCMD->IsInvalid = true;
+					if (((prevCMD->LBA == curCMD->LBA) || (prevCMD->EraseEndLBA == curCMD->EraseEndLBA))
+						|| ((prevCMD->LBA > curCMD->LBA) && (prevCMD->LBA <= curCMD->EraseEndLBA + 1))
+						|| ((prevCMD->LBA < curCMD->LBA) && (prevCMD->EraseEndLBA + 1 >= curCMD->LBA))){
+						//mrege
+						MergeEraseRange(prevCMD, curCMD);
 					}
 				}
 			}
-
 		}
 		int buf_idx = 0;
 		SSDCommand output_array[5];
 		for (int nIter = 0; nIter < 5; nIter++) {
-			if (cmd_array[nIter].IsInvalid == false) {
+			if (cmd_array[nIter].IsValid == true) {
 				output_array[buf_idx] = cmd_array[nIter];
 				buf_idx++;
 			}
 		}
 		for (int nIter = buf_idx; nIter < 5; nIter++) {
-			output_array[nIter].IsInvalid = true;
+			output_array[nIter].IsValid = false;
 		}
 	}
 	SSDCommand cmd_array[5];
