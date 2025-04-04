a48a9fdfd43462752d44727076fa1800ba841b8f
shindongjae <sindj0267@naver.com>
2025-04-04 13:52:22 +0900

    [fix] google style을 적용을 위한 clang-format 추가

--- Shell/mock_ssddriver.h
+++ Shell/mock_ssddriver.h
@@ -1,6 +1,6 @@
 #include "gmock/gmock.h"
-#include "ssddriver_interface.h"
 #include "ssddriver.h"
+#include "ssddriver_interface.h"
 
 #ifndef SSD_DRIVER_MOCK_H_
 #define SSD_DRIVER_MOCK_H_
@@ -8,11 +8,10 @@
 #include <string>
 
 class MockSsdDriver : public SsdDriver {
-public:
+   public:
     MOCK_METHOD(uint32_t, readSSD, (int LBA), (override));
     MOCK_METHOD(void, writeSSD, (int LBA, uint32_t data), (override));
     MOCK_METHOD(bool, executeCmd, (std::string cmdLine), (override));
-
 };
 
-#endif // !SSD_DRIVER_MOCK_H_
+#endif  // !SSD_DRIVER_MOCK_H_
