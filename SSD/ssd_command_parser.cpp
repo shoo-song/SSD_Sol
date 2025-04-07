2f3bdf3f64c077d0c653e7240f1e0c08cfbd9d28
shoo-song <kotlm28@naver.com>
2025-04-07 11:11:46 +0900

    [fix] char fix

--- SSD/ssd_command_parser.cpp
+++ SSD/ssd_command_parser.cpp
@@ -99,19 +99,19 @@ CmdInfo CommandParser::parseArg(int argCount, char CMD, string LBAstring,
 
   Command.LBA = lbaValue;
 
-  if (cmdChar == 'w') {
+  if (cmdChar == 'w' || cmdChar == 'W') {
     strcpy_s(Command.input_data, data);
     Command.CMDType = CMD_WRITE;
   }
   if (cmdChar == 'r' || cmdChar == 'R') {
     Command.CMDType = CMD_READ;
   }
-  if (cmdChar == 'e') {
+  if (cmdChar == 'e' || cmdChar == 'E') {
     Command.EraseEndLBA = Command.LBA + stoi(data) - 1;
     strcpy_s(Command.input_data, data);
     Command.CMDType = CMD_ERASE;
   }
-  if (cmdChar == 'f') {
+  if (cmdChar == 'f' || cmdChar == 'F') {
     Command.CMDType = CMD_FLUSH;
   }
 
